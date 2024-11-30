a147e8ffb29d970b993ec3e89d230261dc019926
shinya.tsukasa <shinya.tsukasa@classmethod.jp>
2024-12-01 06:32:00 +0900

    sandbox aft

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "mototsuka8973+aft-sanbox-aft@gmail.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "mototsuka8973+aft-sanbox-aft@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
